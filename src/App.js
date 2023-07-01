8cd0d204ba20a16f0d7757fa29960e86cd4bc530
Junrong Fu <junrongfu6@gmail.com>
2023-07-01 12:09:42 +0800

    added title & description

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import Form from './components/Form';
+import Image from './assets/1.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title text-3xl font-bold" >Littile Lemon</h1>
+        <p className='city'>Chicago</p>
+        <div className='description-area'>
+          <p className='description'>
+          We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.
+          </p>
+          <div className='description-img'>
+            <img src={Image} alt='restaurant'/>
+          </div>
+        </div>
       </header>
+      <Form/>
     </div>
   );
 }
